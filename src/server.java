5d0de46d342aee66ffdacfcaaca34fa7e54bc899
dbc2201 <kshitij.sahu_cs19@gla.ac.in>
2022-02-27 12:05:44 +0530

    established connection between client and server

--- src/server.java
+++ src/server.java
@@ -1,2 +1,12 @@
+import java.io.IOException;
+import java.net.Socket;
+
 public class server {
+    public static void main(String[] args) throws IOException {
+//        Thread.sleep(1000L);
+        Socket socket = new Socket("localhost", 4000);
+
+        System.out.println("connected to  the server");
+
+    }
 }
