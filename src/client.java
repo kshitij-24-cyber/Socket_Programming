647bf0ce4af430baa1b674cc675118d238c1378c
dbc2201 <kshitij.sahu_cs19@gla.ac.in>
2022-02-27 12:55:07 +0530

    server port created

--- src/client.java
+++ src/client.java
@@ -1,23 +1,5 @@
-import java.io.IOException;
-import java.net.ServerSocket;
-import java.net.Socket;
+public class client {
+    public static void main(String[] args) {
 
-/**
- *
- * What is socket programming in Java?
- * Socket programming in Java is used for communication between the applications that are running on different JRE.
- * It can be either connection-oriented or connectionless.
- * On the whole, a socket is a way to establish a connection between a client and a server
- */
-
-
-class client {
-
-    public static void main(String[] args) throws IOException {
-        ServerSocket serverSocket = new ServerSocket(4000);
-        System.out.println("wait for client to connect with the server :");
-        //waiting to client to connect with  the server
-        Socket clientSocket = serverSocket.accept();
-        System.out.println("connecntion with client sucessfull ");
     }
 }
--- src/server.java
+++ src/server.java
@@ -1,12 +1,13 @@
 import java.io.IOException;
+import java.net.ServerSocket;
 import java.net.Socket;
 
 public class server {
     public static void main(String[] args) throws IOException {
-//        Thread.sleep(1000L);
-        Socket socket = new Socket("localhost", 4000);
+        ServerSocket socket = new ServerSocket(4000);
+        System.out.println("waiting for client to connect with the server");
+        Socket clientSocket = socket.accept();
 
-        System.out.println("connected to  the server");
 
     }
 }
