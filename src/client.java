cf4a462cb27b826f811c0b5074710b05d5ed49ab
dbc2201 <kshitij.sahu_cs19@gla.ac.in>
2022-02-27 13:22:57 +0530

    final commit

--- src/client.java
+++ src/client.java
@@ -3,14 +3,25 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.Scanner;
 
 public class client {
     public static void main(String[] args) throws IOException {
-        Socket  socket = new Socket("localhost",4000);
+        Socket socket = new Socket("localhost", 4000);
         System.out.println("connected with server");
-
+        // bufferReader is used to give input by the client to server
         BufferedReader in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
-        PrintWriter out = new PrintWriter(socket.getOutputStream(),true);
+        PrintWriter out = new PrintWriter(socket.getOutputStream(), true);
+
+        Scanner sc = new Scanner(System.in);
+        String message;
+        do {
+            System.out.println("enter the message you want to send to the server : ");
+            message = sc.nextLine();
+            out.println(message);
+            String response = in.readLine();
+            System.out.println(response);
+        }while (message.equals("exits"));
 
 
     }
